a6c9025e09756694450e8d41377570a851d93736
Priyankara Athapaththu <88779731+priyankara96@users.noreply.github.com>
2022-10-30 14:43:59 +0530

    Update authentication.api.js

--- src/api/authentication.api.js
+++ src/api/authentication.api.js
@@ -10,8 +10,8 @@ const saveUser = async (req, res) => {
       if (user) {res.status(400).send({ message: "failed"}); } else { 
       const commonSignup = new CommonSignup(req.body);
       const savedCommonSignup = await commonSignup.save();
-      const emailTemplate = sendlink(req.body.email); //
-      sendEmail(emailTemplate);
+      // const emailTemplate = sendlink(req.body.email); //
+      // sendEmail(emailTemplate);
       if (savedCommonSignup) {
         res.status(201).send({ message: "success", data: savedCommonSignup });
       } else {
